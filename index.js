1a20c9cc1e9baa6804dae9dc5a697b1600107c89
asrafsujan247 <asraf.sujan247@gmail.com>
2023-06-12 20:36:34 +0600

    installed jwt web token

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 require("dotenv").config();
@@ -70,7 +71,7 @@ async function run() {
     app.patch("/bookings/:id", async (req, res) => {
       const id = req.params.id;
       const booking = req.body;
-      console.log(booking);
+
       const filter = { _id: new ObjectId(id) };
 
       const updatedBooking = {
