4b210a0f1b384279dfeaf1c1039f5e07306023aa
asrafsujan247 <asraf.sujan247@gmail.com>
2023-06-17 21:19:46 +0600

    changed vercel.json config and deployed server side in vercel and changed some codes with jwt in index.js

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ const verifyJWT = (req, res, next) => {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const serviceCollection = client.db("carDoctor").collection("services");
     const bookingCollection = client.db("carDoctor").collection("bookings");
@@ -132,7 +132,7 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
